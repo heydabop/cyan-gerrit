ee98cdb2bd1faf39c2dd24c491aa96e3dae04eb6
heydabop <heydabop@gmail.com>
2015-02-12 16:34:08 -0600

    error messages, print changes slightly cleaner

--- cyan-gerrit.go
+++ cyan-gerrit.go
@@ -24,33 +24,39 @@ func main(){
 	if os.IsNotExist(err) {
 		lastrun = time.Now().AddDate(0, 0, -1)
 	} else if err != nil {
+		fmt.Println("Error reading lastrun: ", err)
 		os.Exit(1)
 	} else {
 		lastrun, err = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", string(lastrunData))
 		if err != nil {
+			fmt.Println("Error parsing lastrun: ", err)
 			os.Exit(2)
 		}
 	}
 
 	resp, err := http.Get("http://review.cyanogenmod.org/changes/?q=status:merged+branch:cm-12.0&n=50")
 	if err != nil {
+		fmt.Println("Error getting JSON data: ", err)
 		os.Exit(3)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		fmt.Println("Error reading JSON data: ", err)
 		os.Exit(4)
 	}
 	body = bytes.TrimPrefix(body, []byte(")]}'\n"))
 	var changes []Gerrit_s
 	err = json.Unmarshal(body, &changes)
 	if err != nil {
+		fmt.Println("Error unmarshalling JSON data: ", err)
 		os.Exit(5)
 	}
+	fmt.Println("Project\t\tSubject\t\tTime")
 	for _, change := range changes {
 		changeTime, err := time.Parse("2006-01-02 15:04:05.000000000", change.Updated)
 		if err == nil && changeTime.After(lastrun) {
-			fmt.Printf("%+v\n", change)
+			fmt.Printf("%s\t\t%s\t\t%s\n", change.Project, change.Subject, changeTime.Format("01-02 15:04"))
 		}
 	}
 
