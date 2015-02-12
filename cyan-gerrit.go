81f7f7b1dda6bd464f8919c71c01d6bacea8c263
heydabop <heydabop@gmail.com>
2015-02-12 12:51:08 -0600

    add file read/write, add age to query. sadly age doesnt do what i thought. after: doesnt seem to exist

--- cyan-gerrit.go
+++ cyan-gerrit.go
@@ -3,19 +3,39 @@ package main
 import(
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main(){
-	resp, err := http.Get("http://review.cyanogenmod.org/changes/?q=status:merged+branch:cm-12.0&n=2")
-	if err != nil {
+	lastrunData, err := ioutil.ReadFile("lastrun")
+	var lastrun time.Time
+	if os.IsNotExist(err) {
+		lastrun = time.Now().AddDate(0, 0, -1)
+	} else if err != nil {
 		os.Exit(1)
+	} else {
+		lastrun, err = time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", string(lastrunData))
+		if err != nil {
+			os.Exit(2)
+		}
+	}
+
+	request := fmt.Sprintf("http://review.cyanogenmod.org/changes/?q=status:merged+branch:cm-12.0+age:%dm", uint64(math.Ceil(time.Since(lastrun).Minutes())))
+	fmt.Println(request)
+	resp, err := http.Get(request)
+	if err != nil {
+		os.Exit(3)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		os.Exit(2)
+		os.Exit(4)
 	}
 	fmt.Println(string(body))
+
+	now := time.Now().String()
+	ioutil.WriteFile("lastrun", []byte(now), 0644)
 }
